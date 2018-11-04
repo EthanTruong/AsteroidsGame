9a7075462cb61eec4d23061b2d273264a21c9073
EthanTruong <ettruong@s.sfusd.edu>
2018-11-04 15:44:43 -0800

    super movement!

--- Spaceship.pde
+++ Spaceship.pde
@@ -1,53 +1,15 @@
 class Spaceship extends Floater { 
+    protected double myTopSpeed;
+
     public Spaceship() {
+        myTopSpeed = 20.0;
         corners = 4;
         int[] xS = {-8, 16, -8, -2};
         int[] yS = {-8, 0, 8, 0};
         xCorners = xS;
         yCorners = yS;
     }
-    public void accel() {
-        boolean wIsPressed = false;
-        if (keyPressed && key == 'w') {
-            wIsPressed = true;
-            System.out.println(wIsPressed);
-        } else {
-            wIsPressed = false;
-            System.out.println(wIsPressed);
-        }
-        if (wIsPressed) {
-            if (myDirectionX < 25 && myDirectionX > -25) {
-                accelerate(0.15, 'x');
-            }
-            if (myDirectionY < 25 && myDirectionY > -25) {
-                accelerate(0.15, 'y');
-            }
-        } else if (!wIsPressed) {
-            if (myDirectionX > 0) {
-                myDirectionX -= 0.25;
-            } else if (myDirectionX < 0) {
-                myDirectionX += 0.25;
-            }
-            if (myDirectionY > 0) {
-                myDirectionY -= 0.25;
-            } else if (myDirectionY < 0) {
-                myDirectionY += 0.25;
-            }
-            if (myDirectionY > -0.1 && myDirectionY < 0.1) {
-                myDirectionY = 0;
-            }
-            if (myDirectionX > -0.1 && myDirectionX < 0.1) {
-                myDirectionX = 0;
-            }
-        }
-        if (keyPressed && key == 'd') {
-            turn(5);
-        } else if (keyPressed && key == 'a') {
-            turn(-5);
-        }
-        System.out.println(myDirectionX);
-        System.out.println(myDirectionY);
-    }
+    
     public void setX(int x) {myCenterX = x;}  
     public int getX() {return (int)myCenterX;}   
     public void setY(int y) {myCenterY = y;}   
@@ -58,4 +20,7 @@ class Spaceship extends Floater {
     public double getDirectionY() {return myDirectionY;}
     public void setPointDirection(int degrees) {myPointDirection = degrees;}   
     public double getPointDirection() {return myPointDirection;}
+    public void setTopSpeed(double topSpeed) {myTopSpeed = topSpeed;}
+    public double getTopSpeedX() {return abs((float)(myTopSpeed * Math.cos(myPointDirection*(Math.PI/180))));}
+    public double getTopSpeedY() {return abs((float)(myTopSpeed * Math.sin(myPointDirection*(Math.PI/180))));}
 }
--- out/source/AsteroidsGame.java
+++ out/source/AsteroidsGame.java
@@ -15,15 +15,93 @@ import java.io.IOException;
 public class AsteroidsGame extends PApplet {
 
 Spaceship Player = new Spaceship();
+boolean wIsPressed, aIsPressed, dIsPressed = false;
+
 public void setup() {
     
+    Player.setTopSpeed(10);
 }
 
 public void draw() {
     background(255);
     Player.show();
     Player.move();
-    Player.accel();
+    accelerate();
+}
+
+public void accelerate() {
+    // DEBUG
+    System.out.println("angle (radians): " + Player.getPointDirection()*(Math.PI/180)); // angle (radians)
+    System.out.println("max x: " + Player.getTopSpeedX() + ",  max y: " + Player.getTopSpeedX()); // converted top speeds
+    System.out.println("speed x: " + Player.getDirectionX() + ",  speed y: " + Player.getDirectionY()); // actual speeds
+    
+    
+    // angles cannot go over 2PI radians
+    if (Player.myPointDirection*(Math.PI/180) > Math.PI*2) {
+        Player.setPointDirection(0);
+    }
+    if (Player.myPointDirection*(Math.PI/180) < -Math.PI*2) {
+        Player.setPointDirection(0);
+    }
+    
+    if (aIsPressed) { Player.turn(-5); }
+    if (dIsPressed) { Player.turn(5); }
+    
+    if (wIsPressed) {
+        if (((Player.getPointDirection()*(Math.PI/180)) < Math.PI/2 && (Player.getPointDirection()*(Math.PI/180)) > -Math.PI/2) || 
+            ((Player.getPointDirection()*(Math.PI/180)) < 2*Math.PI && (Player.getPointDirection()*(Math.PI/180)) > (3*Math.PI)/2)) {
+            if (Player.getDirectionX() < Player.getTopSpeedX()) {
+                Player.accelerate(0.15f, "x");
+            }
+        }
+        if (((Player.getPointDirection()*(Math.PI/180)) < 3*Math.PI/2 && (Player.getPointDirection()*(Math.PI/180)) > Math.PI/2) || 
+            ((Player.getPointDirection()*(Math.PI/180)) < -Math.PI/2 && (Player.getPointDirection()*(Math.PI/180)) > -Math.PI)) {
+            if (Player.getDirectionX() > -Player.getTopSpeedX()) {
+                Player.accelerate(0.15f, "x");
+            }
+        }
+        if (((Player.getPointDirection()*(Math.PI/180)) > Math.PI && (Player.getPointDirection()*(Math.PI/180)) < 2*Math.PI) ||
+            ((Player.getPointDirection()*(Math.PI/180)) > -Math.PI && (Player.getPointDirection()*(Math.PI/180)) < 0)) {
+            if (Player.getDirectionY() > -Player.getTopSpeedY()) {
+                Player.accelerate(0.15f, "y");
+            }
+        }
+        if (((Player.getPointDirection()*(Math.PI/180)) > 0 && (Player.getPointDirection()*(Math.PI/180)) < Math.PI) ||
+            ((Player.getPointDirection()*(Math.PI/180)) > -2*Math.PI && (Player.getPointDirection()*(Math.PI/180)) < -Math.PI)) {
+            if (Player.getDirectionY() < Player.getTopSpeedY()) {
+                Player.accelerate(0.15f, "y");
+            }
+        }
+    } else if (!wIsPressed) {
+        if (Player.getDirectionX() > 0) {
+            Player.accelerate(-0.25f, "x");
+        } else if (Player.getDirectionX() < 0) {
+            Player.accelerate(-0.25f , "x2");
+        }
+        if (Player.getDirectionY() > 0) {
+            Player.accelerate(-0.25f , "y");
+        } else if (Player.getDirectionY() < 0) {
+            Player.accelerate(0.25f , "y2");
+        }
+        if (Player.getDirectionY() > -0.2f && Player.getDirectionY() < 0.2f) {
+            Player.setDirectionY(0);
+        }
+        if (Player.getDirectionX() > -0.2f && Player.getDirectionX() < 0.2f) {
+            Player.setDirectionX(0);
+        }
+    }
+}
+
+public void keyPressed() {
+    if ( key == 'w' ) { wIsPressed = true; } else 
+    if ( key == 'a' ) { aIsPressed = true; } else
+    if ( key == 'd' ) { dIsPressed = true; }
+}
+
+public void keyReleased() {
+    if ( key == 'w' ) { wIsPressed = false; } else 
+    if ( key == 'a' ) { aIsPressed = false; } else
+    if ( key == 'd' ) { dIsPressed = false; }
 }
 abstract class Floater //Do NOT modify the Floater class! Make changes in the Spaceship class 
 {   
@@ -46,22 +124,28 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
   abstract public double getPointDirection(); 
 
   //Accelerates the floater in the direction it is pointing (myPointDirection)   
-  public void accelerate (double dAmount, char direction)
+  public void accelerate (double dAmount, String direction)
   {          
     //convert the current direction the floater is pointing to radians    
-    double dRadians =myPointDirection*(Math.PI/180);     
+    double dRadians = myPointDirection*(Math.PI/180);
     //change coordinates of direction of travel
-        if (direction == 'x') {
+        if (direction == "x") {
             myDirectionX += ((dAmount) * Math.cos(dRadians));
         }
-        if (direction == 'y') {
+        if (direction == "y") {
             myDirectionY += ((dAmount) * Math.sin(dRadians)); 
-        }      
+        }  
+        if (direction == "x2") {
+            myDirectionX += ((dAmount) * Math.cos(-dRadians)); 
+        }     
+        if (direction == "y2") {
+            myDirectionY += ((dAmount) * Math.sin(-dRadians)); 
+        }     
   }   
   public void turn (int nDegreesOfRotation)   
   {     
     //rotates the floater by a given number of degrees    
-    myPointDirection+=nDegreesOfRotation;   
+    myPointDirection+=nDegreesOfRotation;
   }   
   public void move ()   //move the floater in the current direction of travel
   {      
@@ -116,52 +200,17 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
   }   
 } 
 class Spaceship extends Floater { 
+    protected double myTopSpeed;
+
     public Spaceship() {
+        myTopSpeed = 20.0f;
         corners = 4;
         int[] xS = {-8, 16, -8, -2};
         int[] yS = {-8, 0, 8, 0};
         xCorners = xS;
         yCorners = yS;
     }
-    public void accel() {
-        boolean wIsPressed = false;
-        if (keyPressed && key == 'w') {
-            wIsPressed = true;
-            System.out.println(wIsPressed);
-        } else {
-            wIsPressed = false;
-            System.out.println(wIsPressed);
-        }
-        if (wIsPressed) {
-            if (myDirectionX < 10 && myDirectionX > -10) {
-                accelerate(0.15f, 'x');
-            }
-        } else if (!wIsPressed) {
-            if (myDirectionX > 0) {
-                myDirectionX -= 0.25f;
-            } else if (myDirectionX < 0) {
-                myDirectionX += 0.25f;
-            }
-            if (myDirectionY > 0) {
-                myDirectionY -= 0.25f;
-            } else if (myDirectionY < 0) {
-                myDirectionY += 0.25f;
-            }
-            if (myDirectionY > -0.1f && myDirectionY < 0.1f) {
-                myDirectionY = 0;
-            }
-            if (myDirectionX > -0.1f && myDirectionX < 0.1f) {
-                myDirectionX = 0;
-            }
-        }
-        if (keyPressed && key == 'd') {
-            turn(5);
-        } else if (keyPressed && key == 'a') {
-            turn(-5);
-        }
-        System.out.println(myDirectionX);
-        System.out.println(myDirectionY);
-    }
+    
     public void setX(int x) {myCenterX = x;}  
     public int getX() {return (int)myCenterX;}   
     public void setY(int y) {myCenterY = y;}   
@@ -172,12 +221,15 @@ class Spaceship extends Floater {
     public double getDirectionY() {return myDirectionY;}
     public void setPointDirection(int degrees) {myPointDirection = degrees;}   
     public double getPointDirection() {return myPointDirection;}
+    public void setTopSpeed(double topSpeed) {myTopSpeed = topSpeed;}
+    public double getTopSpeedX() {return abs((float)(myTopSpeed * Math.cos(myPointDirection*(Math.PI/180))));}
+    public double getTopSpeedY() {return abs((float)(myTopSpeed * Math.sin(myPointDirection*(Math.PI/180))));}
 }
 class Star //note that this class does NOT extend Floater
 {
   //your code here
 }
-  public void settings() {  size(300, 300); }
+  public void settings() {  size(500, 500); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "AsteroidsGame" };
     if (passedArgs != null) {
